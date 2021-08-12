f170837a1a94bec621cd4a4337e435b3a2941879
rickypoolay <rickypoullet@gmail.com>
2021-08-12 03:14:38 -0400

    Copt now implemented
    need to make it create an empty input box instead that has display none and the shortened inside the value of the temp input and copy that way and delete the temp input so user doesnt see highlighted text

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ let linksCounter = 0;
     }
 
     const generateShortLink = function () {
+        //Set button to display ' Shortening... '
+        $('.shorten-btn').text('Shortening...');
 
         //Gather User Input
         const userInput = $('.user-input').val();
@@ -30,7 +32,9 @@ let linksCounter = 0;
             //If an error is throw add a notification to user for a correct link.
             $('.shortener-container p').toggle();
             $('.user-input').css('border', '5px solid var(--red)');
-            } else {
+        } else {
+            
+                //Reset If Incorrect field to normal
                 $('.shortener-container p').hide();
                 $('.user-input').css('border', 'none');
                 $('.shorten-btn').text('Shortening...');
@@ -46,35 +50,65 @@ let linksCounter = 0;
         const generateHTML = (shortLink) => {
             linksCounter++
 
-            //Set button to display ' Shortening... '
-            $('.shorten-btn').text('Shortening...');
 
             //Shortened link
             const shortenedLink = shortLink.short_link;
             console.log(shortenedLink);
 
-            //Add link to list            
-            const html = 
-            `
-            <div class='link-container link${linksCounter}'>
-            <p class='original-link'>${userInput}</p>
-            <p class='shortened-link'>${shortenedLink}</p>
-            <button class='copy-btn'>Copy</button>
-            </div>
-            `
+            //Add link to list   
+            const linksContainer = $('.links-list-container');
+            
+            //Create link container div
+            const createDiv = document.createElement('div');
+            createDiv.classList.add('link-container');
+            linksContainer.append(createDiv);
+            
+            //Add User Input link
+            const createUserInput = document.createElement('p');
+            createUserInput.classList.add('original-link');
+            createUserInput.innerText = userInput;
+            createDiv.append(createUserInput);
+            
+            //Add Shortened link
+            const createShortenedLink = document.createElement('input');
+            createShortenedLink.classList.add('shortened-link');
+            createShortenedLink.setAttribute('readonly', '');
+            createShortenedLink.value = shortenedLink;
+            createDiv.append(createShortenedLink);
+
+            //Add Copy Button
+            const createCopyBtn = document.createElement('button');
+            createCopyBtn.classList.add('copy-btn');
+            createCopyBtn.innerText = 'Copy';
+            createDiv.append(createCopyBtn);
+
+            createCopyBtn.addEventListener('click', () => {
+                createShortenedLink.select();
+                createShortenedLink.setSelectionRange(0, 99999);
+                document.execCommand('copy');
+
+                $(createCopyBtn).text('Copied!');
+                $(createCopyBtn).css('background-color' , 'var(--bg)');
+            }) 
+
+            
+
+            
+
+            // const html = 
+            // `
+            // <div class='link-container link${linksCounter}'>
+            // <p class='original-link'>${userInput}</p>
+            // <p class='shortened-link'>${shortenedLink}</p>
+            // <button class='copy-btn'>Copy</button>
+            // </div>
+            // `
             //Add Html to Container
-            $('.links-list-container').append(html);
+            // $('.links-list-container').append(html);
 
-            //Eventlistener to copy button
-            $(`.link-list-container link${linksCounter} .copy-btn`).click(() => {
-                console.log('yooo');
-            })
-
+            //Reset Button Text
             $('.shorten-btn').text('Shorten It!');
         }
-
-        //Reset Button Text
-        
 
     }
 
@@ -85,5 +119,5 @@ let linksCounter = 0;
     $('.bg').click(navToggle);
 
     //Generate Link
-    $('.shorten-btn').click(generateShortLink, );
+    $('.shorten-btn').click(generateShortLink);
 });
